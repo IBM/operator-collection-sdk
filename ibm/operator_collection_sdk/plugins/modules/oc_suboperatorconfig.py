d62bd86d064104e1bc9ca7ec3cc39343e9a3b30b
zarin tasnim ohiba <zarinohiba@zarins-mbp.usca.ibm.com>
2024-02-20 12:09:16 -0800

    suboperatorconfig module added

---
+++ ibm/operator_collection_sdk/plugins/modules/oc_suboperatorconfig.py
@@ -0,0 +1,195 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2024, Zarin Ohiba <zarin.ohiba@ibm.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+import kubernetes
+import csv, os, json, yaml
+from jinja2 import Environment, FileSystemLoader
+
+from kubernetes import client, config, watch
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: oc_suboperatorconfig
+
+short_description: This module creates a suboperatorconfig
+
+version_added: "1.0.0"
+
+description: This module creates a suboperatorconfig.
+
+options:
+    state:
+        description: Determines if an object should be created, patched, or deleted. When set to C(present), an object will be created, if it does not already exist. If set to C(absent), an existing object will be deleted. If set to C(present), an existing object will be patched, if its attributes differ from those specified using I(resource_definition) or I(src). C(patched) state is an existing resource that has a given patch applied. If the resource doesn't exist, silently skip it (do not raise an error).
+        type: str
+        required: true
+        default: present
+        choices: [ absent, present, patched ]
+    name:
+        description: The name of the zosendpoint.
+        required: true
+        type: str
+    namespace:
+        description: The endpoint is created in this provided namespace.
+        required: true
+        type: str
+    credentialType:
+        description: Sets credential type to shared or personal.
+        required: true
+        type: str
+    collection_src:
+        description: Sets location of the collection source.
+        required: true
+        type: str
+    
+
+    
+# Specify this value according to your collection
+# in format of namespace.collection.doc_fragment_name
+# extends_documentation_fragment:
+#     - ocsdk.collection.my_doc_fragment_name
+
+author:
+    - Your Name (@zohiba)
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+- name: Test with shared suboperatorconfig
+  ocsdk.collection.oc_suboperatorconfig:
+    name: endpoint-1
+    namespace: test-world
+    credentialType: shared
+    
+    state: present
+
+# pass in a message and have changed true
+- name: Test with personal suboperatorconfig
+  ocsdk.collection.oc_suboperatorconfig:
+    name: endpoint-2
+    namespace: test-world
+    state: present
+    credentialType: personal
+# fail the module
+- name: Test failure of the module
+  ocsdk.collection.oc_suboperatorconfig:
+    name: endpoint-3
+    namespace: test-world
+    state: blah
+    endpointType: blah
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+resource:
+    description: The resource that was modified.
+    type: str
+    returned: always
+   
+errMsg:
+    description: The error message that the test module generates.
+    type: str
+    returned: always
+   
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        name=dict(type='str', required=True),
+        namespace=dict(type='str', required=True),
+        collection_src=dict(type='str', required=True),
+        credentialType=dict(type='str', required=True, choices=["personal", "shared" ]),
+        endpointMapping=dict(type="list", required=True),
+        state=dict(type='str', required=True, choices=[ "absent", "present", "patched" ]),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and resource
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        resource='',
+        error=False,
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+    )
+
+    #config.load_incluster_config() #used during testing inside cluster, also needs to give permission to SA to list endpoints
+    # config.load_incluster_config()
+    # v1 = client.CoreV1Api()
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+
+    environment = Environment(loader=FileSystemLoader("./templates"))
+    template = environment.get_template("suboperatorconfig.yml")
+    content = template.render(
+        module.params
+    )
+    yaml_object = yaml.safe_load(content)
+
+    # # this is going to be the part where your module will do what it needs to do
+    crd_resource, changed, err, errorMsg = suboperatorconfig_action(module, yaml_object)
+    # # if the user is working with this module in only check mode we do not
+    # # want to make any changes to the environment, just return the current
+    # # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    result['resource'] = crd_resource
+    result['error'] = err
+    result['changed'] = changed
+
+    if err:
+        module.fail_json(msg=errorMsg, **result)
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def suboperatorconfig_action(module, yaml_object):
+    # Given the module(params,kubeconfig etc) check the desired state of the resource and apply the kubernetes API accordingly.
+    crd_resource=""
+    err= False
+    changed = False
+    try:
+        if module.params["state"] == "present":
+            crd_resource = kubernetes.client.CustomObjectsApi().create_namespaced_custom_object(group="zoscb.ibm.com", version="v2beta2", plural="suboperatorconfigs", body=yaml_object, namespace=module.params["namespace"])
+            changed = True
+        elif module.params["state"] == "absent":
+            crd_resource = kubernetes.client.CustomObjectsApi().delete_namespaced_custom_object(group="zoscb.ibm.com", version="v2beta2", plural="suboperatorconfigs", name=module.params["name"] , namespace=module.params["namespace"])
+            changed = True
+        elif module.params["state"] == "patched":
+            crd_resource = kubernetes.client.CustomObjectsApi().patch_namespaced_custom_object(group="zoscb.ibm.com", version="v2beta2", plural="suboperatorconfigs", name=module.params["name"] ,body=yaml_object, namespace=module.params["namespace"])
+            changed = True
+    
+        return crd_resource,changed, err, None
+    except Exception as e:
+        err = True
+        return crd_resource, changed, err, str(e)
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
